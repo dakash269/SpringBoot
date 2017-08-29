121385ff2d37caafab229c9d1b23dbfe651cb50d
dakash269 <dakash269@gmail.com>
2017-08-29 18:11:12 +0530

    Update FileFinder.java

--- FileFinder.java
+++ FileFinder.java
@@ -4,19 +4,19 @@ class FileFinder {
    public static void main(String[] args) {
         FileFinder fileFinder = new FileFinder();
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.next();                      //taking filename as input from keyboard
+        String name = scanner.next();                                       //taking filename as input from keyboard
         String directory = "/home";
-        fileFinder.findFile( name, new File(directory));          //calling findFile method
+        fileFinder.findFile( name, new File(directory));                    //calling findFile method
     }
     public void findFile(String name,File file) {
         File[] list = file.listFiles();
         if(list!= null) {                                          
            for (File fil : list) {
-		       if (fil.isDirectory()) {                             //going inside directory  
+		       if (fil.isDirectory()) {                              //going inside directory  
 		          findFile(name,fil); 
 		       }
 		       else if (name.equalsIgnoreCase(fil.getName())) {      //ignoring cases in file name while searching
-		          System.out.println(fil.getAbsolutePath());       //printing absolute path of file
+		          System.out.println(fil.getAbsolutePath());         //printing absolute path of file
 		       }
            }
         }
