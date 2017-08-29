8d5eb4429d2df3599cf266736837980d68f8df26
dakash269 <dakash269@gmail.com>
2017-08-29 17:35:45 +0530

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -1,42 +1,29 @@
 import java.util.*;
 import java.lang.*;
-class Q2
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-             Scanner sc = new Scanner(System.in);  
-             String s = sc.next();                                //taking input from keyboard
-             Set<Integer> mySet = new HashSet<>();                //Creating new hashset
-             for(int i = 0; i < s.length(); i++)
-	     {
-	             int ascii = (int) s.charAt(i);
-	             if(ascii >= 65 && ascii <= 90) { mySet.add(ascii);}      //if character is lowercase-alphabet
-	             if(ascii >= 97 && ascii <= 122){ mySet.add(ascii - 32);} //if character is uppercase-alphabet
+class Q2 {
+	public static void main (String[] args) throws java.lang.Exception {
+             Scanner scanner = new Scanner(System.in);  
+             String string = scanner.next();                                //taking input from keyboard
+             Set<Integer> alphabetSet = new HashSet<>();                //Creating new hashset
+             for(int i = 0; i < string.length(); i++) {
+	             int ascii = (int) string.charAt(i);
+	             if(ascii >= 65 && ascii <= 90) {  // ascii value of 'a' is 65 and 'z' is 90. other alphabets have ascii value between 65 and 90 in alphabetic order
+			     alphabetSet.add(ascii);
+		     }      //if character is lowercase-alphabet
+	             if(ascii >= 97 && ascii <= 122) { // ascii value of 'A' is 97 and 'Z' is 132. other alphabets have ascii value between 65 and 90 in alphabetic order
+			     alphabetSet.add(ascii - 32);
+		     } //if character is uppercase-alphabet
 	     }
-	           Iterator iter = mySet.iterator();
-	           int sum = 0;
-                   while (iter.hasNext()) { sum = sum + (int) iter.next();} //summing up all the entries into mySet
-                   if(sum == 2015) { System.out.println("yes");}            //if sum of 65,66,....90=2015 then all alphabets are there
-                   else { System.out.println("no");}
+	     Iterator iterator = alphabetSet.iterator();
+	     int sumOfalphabetSet = 0;
+	     while (iterator.hasNext()) { 
+		     sumOfalphabetSet = sumOfalphabetSet + (int) iterator.next();
+	     }               //summing up all the entries into alphabetSet
+	     if(sumOfalphabetSet == 2015) { 
+		   System.out.println("yes, all 26 alphabets are in input string. ");
+	     }            //if sum of 65,66,....90=2015 then all alphabets are there
+	     else { 
+		   System.out.println("no, all 26 alphabets are not in input string. ");
+	     }
 	}
 }
-/*test cases:
-1. 
-input:abcdefghijklmnopqrstuvwxyz
-output:yes
-2.
-input:ABCDEFGHIJKLMNOPQRSTUVWXYZ
-output:yes
-3.
-input:abcdefghijklmnopqrstuvwxyzMNOPQRSTUVWXYZ
-output:yes
-4.
-input:abcdefghijklmnopqrstuvw
-output:no
-5.
-input:abcdefghijklmnopqrstuvabcdef
-output:no
-6.
-input:abcdefghijkl+678mnopqrstuvabcdef
-output:no
-*/
