b6008a6e5e3846680ad2fcee1b3dc2d522b1e663
dakash269 <dakash269@gmail.com>
2017-08-29 18:05:13 +0530

    Update FileFinder.java

--- FileFinder.java
+++ FileFinder.java
@@ -10,7 +10,7 @@ class FileFinder {
     }
     public void findFile(String name,File file) {
         File[] list = file.listFiles();
-        if(list!=null) {                                          
+        if(list!= null) {                                          
            for (File fil : list) {
 		       if (fil.isDirectory()) {                             //going inside directory  
 		          findFile(name,fil); 
