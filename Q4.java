4bb273bfc3932270192e852a2281ae3202d695db
dakash269 <dakash269@gmail.com>
2017-08-29 13:39:07 +0530

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -9,27 +9,39 @@ class Q4
     
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	Scanner sc=new Scanner(System.in);
-	int n=sc.nextInt();
-	for(int j=0;j<n;j++){
-		String s1=sc.next();String s2=sc.next();
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();                   //number of test cases as input
+	for(int j = 0;j < n; j++){
+		String s1 = sc.next();          //taking signup-date as input
+		String s2 = sc.next();          //taking current-date as input
 	            SimpleDateFormat f = new SimpleDateFormat("dd-MM-yyyy");
-	            Date date1 = f.parse(s1);Date date2 = f.parse(s2);
-	            Calendar calendar2 = new GregorianCalendar();calendar2.setTime(date2);int year2 = calendar2.get(Calendar.YEAR);
-	            Calendar calendar1 = new GregorianCalendar();calendar1.setTime(date1);int year1 = calendar1.get(Calendar.YEAR);
-	            calendar1.add(GregorianCalendar.YEAR,year2-year1);
-	            String s3=f.format( calendar1.getTime());Date date3 = f.parse(s3);
+	            Date date1 = f.parse(s1);   //convert signup-date into date-format
+		    Date date2 = f.parse(s2);   //convert current-date into date-format
+	            Calendar calendar2 = new GregorianCalendar();
+		    calendar2.setTime(date2);
+		    int year2 = calendar2.get(Calendar.YEAR);    //calculating year of current-date
+	            Calendar calendar1 = new GregorianCalendar();
+		    calendar1.setTime(date1);
+		    int year1 = calendar1.get(Calendar.YEAR);   //calculating year of signup-date
+	            calendar1.add(GregorianCalendar.YEAR,year2-year1);  //changing signup-date to its anniversary
+	            String s3 = f.format( calendar1.getTime());
+		    Date date3 = f.parse(s3);                 //convert signup-anniversary into date-format
 		            Calendar cal1 = GregorianCalendar.getInstance();
 		            cal1.setTime(date3);
-		            cal1.add( GregorianCalendar.DATE, -30 );
+		            cal1.add( GregorianCalendar.DATE, -30 );   //calculating -30 days ahead date of signup-anniversary    
 		            Calendar cal2 = GregorianCalendar.getInstance();
 		            cal2.setTime(date3);
-		            cal2.add( GregorianCalendar.DATE, 30 );
-		String s4=f.format( cal1.getTime());Date date4 = f.parse(s4);
-		String s5=f.format( cal2.getTime());Date date5 = f.parse(s5);
-			if(date2.compareTo(date4)>=0 && date2.compareTo(date5)>=0) System.out.println(s4+" "+s5);
-			if(date2.compareTo(date4)>0 && date2.compareTo(date5)<0) System.out.println(s4+" "+s2);
-		            if(date2.compareTo(date4)<=0 && date2.compareTo(date5)<=0) System.out.println("No range");
+		            cal2.add( GregorianCalendar.DATE, 30 );    //calculating +30 days ahead date of signup-anniversary
+		   String s4 = f.format( cal1.getTime());
+		   Date date4 = f.parse(s4);              //convert signupdate-30 into date-format
+		   String s5 = f.format( cal2.getTime());
+		   Date date5 = f.parse(s5);              //convert signupdate+30 into date-format
+			if(date2.compareTo(date4) >= 0 && date2.compareTo(date5) >= 0) System.out.println(s4 + " " + s5);
+		//if current-date is after that 60 days window
+			if(date2.compareTo(date4) > 0 && date2.compareTo(date5) < 0) System.out.println(s4 + " " + s2);
+		//if current-date is between that 60 days window
+		        if(date2.compareTo(date4) <= 0 && date2.compareTo(date5) <= 0) System.out.println("No range");
+		//if current-date is before that 60 days window
 	  }
 	}
 }
