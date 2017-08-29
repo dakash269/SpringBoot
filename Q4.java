e82bd20a62276bb4ee82fdf29968ca22da46c84c
dakash269 <dakash269@gmail.com>
2017-08-29 17:28:28 +0530

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -7,14 +7,14 @@ import java.util.GregorianCalendar;
 class Q4
 {
 	public static void main (String[] args) throws java.lang.Exception {
-	  Scanner sc = new Scanner(System.in);
-	  int n = sc.nextInt();                   //number of test cases as input
+	  Scanner scanner = new Scanner(System.in);
+	  int n = scanner.nextInt();                   //number of test cases as input
 	  for(int j = 0;j < n; j++) {
-		String signUp = sc.next();          //taking signup-date as input
-		String current = sc.next();          //taking current-date as input
-	        SimpleDateFormat f = new SimpleDateFormat("dd-MM-yyyy");
-	        Date signUpDate = f.parse(signUp);   //convert signup-date into date-format
-		Date currentDate = f.parse(current);   //convert current-date into date-format
+		String signUp = scanner.next();          //taking signup-date as input
+		String current = scanner.next();          //taking current-date as input
+	        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+	        Date signUpDate = simpleDateFormat.parse(signUp);   //convert signup-date into date-format
+		Date currentDate = simpleDateFormat.parse(current);   //convert current-date into date-format
 	        Calendar currentDateInstance = new GregorianCalendar();
 		currentDateInstance.setTime(currentDate);
 	        int currentDateYear = currentDateInstance.get(Calendar.YEAR);    //calculating year of current-date
@@ -24,18 +24,18 @@ class Q4
 		if(currentDateYear>=signUpDateYear) {
 	                signUpDateInstance.add(GregorianCalendar.YEAR,currentDateYear - signUpDateYear);  //changing signup-date to its anniversary
 		}
-	        String s3 = f.format( signUpDateInstance.getTime());
-	        Date date3 = f.parse(s3);                 //convert signup-anniversary into date-format
+	        String signUpAnniversary = simpleDateFormat.format( signUpDateInstance.getTime());
+	        Date signUpAnniversaryDate = simpleDateFormat.parse(signUpAnniversary);                 //convert signup-anniversary into date-format
 	        Calendar beforeSignUpDateInstance = GregorianCalendar.getInstance();
-		beforeSignUpDateInstance.setTime(date3);
+		beforeSignUpDateInstance.setTime(signUpAnniversaryDate);
 		beforeSignUpDateInstance.add( GregorianCalendar.DATE, -30 );   //calculating -30 days ahead date of signup-anniversary    
 		Calendar afterSignUpDateInstance = GregorianCalendar.getInstance();
-		afterSignUpDateInstance.setTime(date3);
+		afterSignUpDateInstance.setTime(signUpAnniversaryDate);
 		afterSignUpDateInstance.add( GregorianCalendar.DATE, 30 );    //calculating +30 days ahead date of signup-anniversary
-		String beforeSignUp = f.format( beforeSignUpDateInstance.getTime());
-		Date beforeSignUpDate = f.parse(beforeSignUp);              //convert signupdate-30 into date-format
-	        String afterSignUp = f.format( afterSignUpDateInstance.getTime());
-	        Date afterSignUpDate = f.parse(afterSignUp);              //convert signupdate+30 into date-format
+		String beforeSignUp = simpleDateFormat.format( beforeSignUpDateInstance.getTime());
+		Date beforeSignUpDate = simpleDateFormat.parse(beforeSignUp);              //convert signupdate-30 into date-format
+	        String afterSignUp = simpleDateFormat.format( afterSignUpDateInstance.getTime());
+	        Date afterSignUpDate = simpleDateFormat.parse(afterSignUp);              //convert signupdate+30 into date-format
                 if(currentDateYear >= signUpDateYear) {
 			  if(currentDate.compareTo(beforeSignUpDate) >= 0 && currentDate.compareTo(afterSignUpDate) >= 0) { 
 			  	System.out.println(beforeSignUp + " " + afterSignUp);
