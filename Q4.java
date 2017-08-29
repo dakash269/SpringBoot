69046cc96053d830b73c13dab714a368bbdf5632
dakash269 <dakash269@gmail.com>
2017-08-29 17:35:26 +0530

    Update Q4.java

--- Q4.java
+++ Q4.java
@@ -4,17 +4,16 @@ import java.text.SimpleDateFormat;
 import java.util.Date;  
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-class Q4
-{
+class Q4 {
 	public static void main (String[] args) throws java.lang.Exception {
 	  Scanner scanner = new Scanner(System.in);
 	  int n = scanner.nextInt();                   //number of test cases as input
 	  for(int j = 0;j < n; j++) {
-		String signUp = scanner.next();          //taking signup-date as input
-		String current = scanner.next();          //taking current-date as input
+		String signUpString = scanner.next();          //taking signup-date as input
+		String currentString = scanner.next();          //taking current-date as input
 	        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
-	        Date signUpDate = simpleDateFormat.parse(signUp);   //convert signup-date into date-format
-		Date currentDate = simpleDateFormat.parse(current);   //convert current-date into date-format
+	        Date signUpDate = simpleDateFormat.parse(signUpString);   //convert signup-date into date-format
+		Date currentDate = simpleDateFormat.parse(currentString);   //convert current-date into date-format
 	        Calendar currentDateInstance = new GregorianCalendar();
 		currentDateInstance.setTime(currentDate);
 	        int currentDateYear = currentDateInstance.get(Calendar.YEAR);    //calculating year of current-date
@@ -24,25 +23,25 @@ class Q4
 		if(currentDateYear>=signUpDateYear) {
 	                signUpDateInstance.add(GregorianCalendar.YEAR,currentDateYear - signUpDateYear);  //changing signup-date to its anniversary
 		}
-	        String signUpAnniversary = simpleDateFormat.format( signUpDateInstance.getTime());
-	        Date signUpAnniversaryDate = simpleDateFormat.parse(signUpAnniversary);                 //convert signup-anniversary into date-format
+	        String signUpAnniversaryString = simpleDateFormat.format( signUpDateInstance.getTime());
+	        Date signUpAnniversaryDate = simpleDateFormat.parse(signUpAnniversaryString);                 //convert signup-anniversary into date-format
 	        Calendar beforeSignUpDateInstance = GregorianCalendar.getInstance();
 		beforeSignUpDateInstance.setTime(signUpAnniversaryDate);
 		beforeSignUpDateInstance.add( GregorianCalendar.DATE, -30 );   //calculating -30 days ahead date of signup-anniversary    
 		Calendar afterSignUpDateInstance = GregorianCalendar.getInstance();
 		afterSignUpDateInstance.setTime(signUpAnniversaryDate);
 		afterSignUpDateInstance.add( GregorianCalendar.DATE, 30 );    //calculating +30 days ahead date of signup-anniversary
-		String beforeSignUp = simpleDateFormat.format( beforeSignUpDateInstance.getTime());
-		Date beforeSignUpDate = simpleDateFormat.parse(beforeSignUp);              //convert signupdate-30 into date-format
-	        String afterSignUp = simpleDateFormat.format( afterSignUpDateInstance.getTime());
-	        Date afterSignUpDate = simpleDateFormat.parse(afterSignUp);              //convert signupdate+30 into date-format
+		String beforeSignUpString = simpleDateFormat.format( beforeSignUpDateInstance.getTime());
+		Date beforeSignUpDate = simpleDateFormat.parse(beforeSignUpString);              //convert signupdate-30 into date-format
+	        String afterSignUpString = simpleDateFormat.format( afterSignUpDateInstance.getTime());
+	        Date afterSignUpDate = simpleDateFormat.parse(afterSignUpString);              //convert signupdate+30 into date-format
                 if(currentDateYear >= signUpDateYear) {
 			  if(currentDate.compareTo(beforeSignUpDate) >= 0 && currentDate.compareTo(afterSignUpDate) >= 0) { 
-			  	System.out.println(beforeSignUp + " " + afterSignUp);
+			  	System.out.println(beforeSignUpString + " " + afterSignUpString);
 			  }
 				//if current-date is after that 60 days window
 			  if(currentDate.compareTo(beforeSignUpDate) > 0 && currentDate.compareTo(afterSignUpDate) < 0) { 
-			  	System.out.println(beforeSignUp + " " + current);
+			  	System.out.println(beforeSignUpString + " " + currentString);
 			  }
 				//if current-date is between that 60 days window
 			  if(currentDate.compareTo(beforeSignUpDate) <= 0 && currentDate.compareTo(afterSignUpDate) <= 0) { 
