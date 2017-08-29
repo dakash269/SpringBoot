4fbf797eb9da8a9ec1536013e13cd2b3bca86921
dakash269 <dakash269@gmail.com>
2017-08-29 17:53:41 +0530

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,31 +1,29 @@
-import java.io.*;
-import java.util.*;
-class Q1 
-{
-   public static void main(String[] args) 
-    {
-        Q1 obj = new Q1();
-        Scanner scan = new Scanner(System.in);
-        String name = scan.next();                      //taking filename as input from keyboard
+import java.io.File;
+import java.util.Scanner;
+class FileFinder {
+   public static void main(String[] args) {
+        FileFinder fileFinder = new FileFinder();
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();                      //taking filename as input from keyboard
         String directory = "/home";
-        obj.findFile(name,new File(directory));          //calling findFile method
+        fileFinder.findFile( name, new File(directory));          //calling findFile method
     }
-    public void findFile(String name,File file)
-    {
+    public void findFile(String name,File file) {
         File[] list = file.listFiles();
-        if(list!=null)                                           
-        for (File fil : list) {
-            if (fil.isDirectory()) {                             //going inside directory  
-                 findFile(name,fil); 
-               }
-            else if (name.equalsIgnoreCase(fil.getName())) {      //ignoring cases in file name while searching
-                 System.out.println(fil.getAbsolutePath());       //printing absolute path of file
-               }
+        if(list!=null) {                                          
+           for (File fil : list) {
+		       if (fil.isDirectory()) {                             //going inside directory  
+		          findFile(name,fil); 
+		       }
+		       else if (name.equalsIgnoreCase(fil.getName())) {      //ignoring cases in file name while searching
+		          System.out.println(fil.getAbsolutePath());       //printing absolute path of file
+		       }
+           }
         }
     }
 }
 /*
-Test cases will depend on your laptop/dekktop 
+Test cases will depend on your laptop/desktop 
 My test cases are:
 1.
 input: hi
