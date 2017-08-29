c0560f749205688fc2fa664d669e62839693cb5a
dakash269 <dakash269@gmail.com>
2017-08-29 15:09:30 +0530

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -24,4 +24,21 @@ class Q1
         }
     }
 }
+/*
+Test cases will depend on your laptop/dekktop 
+My test cases are:
+1.
+input: hi
+output:
+/home/zemoso/Pictures/hi
+/home/zemoso/Downloads/hi
+2.
+input:E07
+output:
+/home/zemoso/Downloads/E07
+
+3.
+input:e07
+output:/home/zemoso/Downloads/E07
+*/
 //we can use any regular expression pattern like only jpg file should be searched
