703901db54e1a2fa9bd27bfec1c6cbc231176dc3
dakash269 <dakash269@gmail.com>
2017-08-29 18:10:30 +0530

    Update AlphabetSetFinder.java

--- AlphabetSetFinder.java
+++ AlphabetSetFinder.java
@@ -5,25 +5,29 @@ import java.util.Scanner;
 class AlphabetSetFinder {
 	public static void main (String[] args) {
              Scanner scanner = new Scanner(System.in);  
-             String string = scanner.next();                                //taking input from keyboard
+             String string = scanner.next();                            //taking input from keyboard
              Set<Integer> alphabetSet = new HashSet<>();                //Creating new hashset
              for(int i = 0; i < string.length(); i++) {
 	             int ascii = (int) string.charAt(i);
-	             if(ascii >= 65 && ascii <= 90) {  // ascii value of 'a' is 65 and 'z' is 90. other alphabets have ascii value between 65 and 90 in alphabetic order
+		     // ascii value of 'a' is 65 and 'z' is 90. other alphabets have ascii value between 65 and 90 in alphabetic order
+	             if(ascii >= 65 && ascii <= 90) {  
 			     alphabetSet.add(ascii);
-		     }      //if character is lowercase-alphabet
-	             if(ascii >= 97 && ascii <= 122) { // ascii value of 'A' is 97 and 'Z' is 132. other alphabets have ascii value between 65 and 90 in alphabetic order
+		     }      
+		     // ascii value of 'A' is 97 and 'Z' is 132. other alphabets have ascii value between 65 and 90 in alphabetic order
+	             if(ascii >= 97 && ascii <= 122) { 
 			     alphabetSet.add(ascii - 32);
-		     } //if character is uppercase-alphabet
+		     } 
 	     }
 	     Iterator iterator = alphabetSet.iterator();
 	     int sumOfalphabetSet = 0;
+	     //summing up all the entries into alphabetSet
 	     while (iterator.hasNext()) { 
 		     sumOfalphabetSet = sumOfalphabetSet + (int) iterator.next();
-	     }               //summing up all the entries into alphabetSet
+	     } 
+	     //if sum of 65,66,....90=2015 then all alphabets are there
 	     if(sumOfalphabetSet == 2015) { 
 		   System.out.println("yes, all 26 alphabets are in input string. ");
-	     }            //if sum of 65,66,....90=2015 then all alphabets are there
+	     }            
 	     else { 
 		   System.out.println("no, all 26 alphabets are not in input string. ");
 	     }
