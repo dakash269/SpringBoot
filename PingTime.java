5de9b55645df20f582658990d1fbffe5e753f1e0
dakash269 <dakash269@gmail.com>
2017-08-30 11:37:34 +0530

    Update PingTime.java

--- PingTime.java
+++ PingTime.java
@@ -14,16 +14,19 @@ class PingTime {
            Scanner scanner = new Scanner(System.in);
 	   System.out.println("Please Enter Host Ip Address");
 	   String ip = scanner.next();
+           // Only if ip address is valid
            if(ip.matches(pattern)) {
 		   System.out.println("How many times you want to ping the host ?");
+		   // Only if number of times to ping is integer
 		   if(scanner.hasNextInt()) {
 			   int n = scanner.nextInt();
 			   ArrayList<Long> numArray = new ArrayList<Long>();
 			   for(int i=0 ; i<n ; i++) {
-			      numArray.add(ping(ip)); 
+			      numArray.add(ping(ip)); // adding pingTime to an array
 			   }
-			   Collections.sort(numArray);
-			   double median = 0;
+			   Collections.sort(numArray);  //Sorting pingTime array
+			   double median = 0;  
+			   // Calculating meadian of pingTimes
 			   if (n % 2 == 0) {
 				    median = ((double)numArray.get(n/2) + (double) numArray.get((n/2 - 1)))/2;
 			   }
@@ -45,10 +48,10 @@ class PingTime {
 	    try {    
                  InetAddress inetAddress = InetAddress.getByName(entryIpAddress);
                  Date start = new Date();
+		        // Only if ip address is reachable
 			if (inetAddress.isReachable(1000)) {
 				Date stop = new Date();
-                                pingtime = stop.getTime() - start.getTime();
-				//System.out.println(stop.getTime() - start.getTime());
+                                pingtime = stop.getTime() - start.getTime(); //Calculating pingTime
 			}
                         else {
                               System.out.println("Host is not reachable");
