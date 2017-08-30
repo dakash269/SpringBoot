af3b0f613b88107113128747854e01b9aaf69bfd
zemoso <dakash269@gmail.com>
2017-08-30 11:30:45 +0530

    Files Updated

--- PingTime.java
+++ PingTime.java
@@ -3,22 +3,42 @@ import java.util.Scanner;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.net.InetAddress;
+import java.util.regex.Matcher;
 class PingTime {
 	public static void main (String[] args) {
+           String pattern =
+		"^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
            Scanner scanner = new Scanner(System.in);
-           int n = scanner.nextInt();
-           ArrayList<Long> numArray = new ArrayList<Long>();
-           for(int i=0 ; i<n ; i++)
-           numArray.add(ping("10.0.0.13")); 
-           Collections.sort(numArray);
-           double median = 0;
-	   if (n % 2 == 0) {
-	 	    median = ((double)numArray.get(n/2) + (double) numArray.get((n/2 - 1)))/2;
+	   System.out.println("Please Enter Host Ip Address");
+	   String ip = scanner.next();
+           if(ip.matches(pattern)) {
+		   System.out.println("How many times you want to ping the host ?");
+		   if(scanner.hasNextInt()) {
+			   int n = scanner.nextInt();
+			   ArrayList<Long> numArray = new ArrayList<Long>();
+			   for(int i=0 ; i<n ; i++) {
+			      numArray.add(ping(ip)); 
+			   }
+			   Collections.sort(numArray);
+			   double median = 0;
+			   if (n % 2 == 0) {
+				    median = ((double)numArray.get(n/2) + (double) numArray.get((n/2 - 1)))/2;
+			   }
+			   else {
+				    median = (double) numArray.get(n/2);
+			   }
+			   System.out.println(median+" ms");
+		    }
+		    else {
+			   System.out.println("Number of times to ping should be integer");
+		    }
+	   } 
+           else {
+               System.out.println("Ip Address is in not valid");
            }
- 	   else {
-	            median = (double) numArray.get(n/2);
-           }
-	   System.out.println(median); 
 	}
        	public static long ping (String entryIpAddress){
             long pingtime = 0;
@@ -28,8 +48,12 @@ class PingTime {
 			if (inetAddress.isReachable(1000)) {
 				Date stop = new Date();
                                 pingtime = stop.getTime() - start.getTime();
-				System.out.println(stop.getTime() - start.getTime());
+				//System.out.println(stop.getTime() - start.getTime());
 			}
+                        else {
+                              System.out.println("Host is not reachable");
+                              pingtime = -1;
+                        }
                  return pingtime;
 	    }
             catch(Exception e) {
@@ -38,4 +62,5 @@ class PingTime {
             return 0;
          }
 }
+
 
